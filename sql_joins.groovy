e2aa568527e3d9f92feac837ab3fce97a45dfa72
Ruslan A <ruslan.absalyamov@gmail.com>
2016-03-02 00:37:54 +0300

    Added sql_joins.groovy - inner join

---
+++ sql_joins.groovy
@@ -0,0 +1,54 @@
+import groovy.transform.Canonical;
+
+
+AuthorRow[] authors = [[1, "Charles Darwin"], 
+					   [2, "George Orwell"],
+					   [3, "Richard Dawkins"],];
+BooksRow[] books = [[10, "On the Origin of Species", 1],
+					[11, "Nineteen Eighty-Four", 2],
+					[12, "Foundation", 4]];
+
+ResultRow[] result = execInnerJoin(books, authors)
+
+printRows(result)
+
+ResultRow[] execInnerJoin(BooksRow[] books, AuthorRow[] authors){
+	ResultRow[] result = [];
+
+	for(def book in books) {
+		for(def author in authors) {
+			if (book.authorId == author.id) {
+				result += new ResultRow(book.id, book.name, author.name)
+			}
+		}		
+	}
+
+	result
+}
+
+@Canonical
+class BooksRow {
+	int id;
+	String name;
+	int authorId;
+}
+
+@Canonical
+class AuthorRow {
+	int id;
+	String name;
+}
+
+@Canonical
+class ResultRow {
+	int bookId;
+	String book;
+	String author
+}
+
+
+void printRows(ResultRow[] rows) {
+	for(def row in rows) {
+		println "$row.bookId\t$row.book\t$row.author"
+	}
+}
